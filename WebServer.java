24d4cdb4c90a48844670714d27664b1dcf79baee
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-04-04 08:15:10 +0100

    Beautified document

--- WebServer.java
+++ WebServer.java
@@ -21,13 +21,13 @@ public class WebServer {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-			String query = t.getRequestURI().getQuery();
-			Map<String, String> args = queryToMap(query);
-			String response = "###\n";
+            String query = t.getRequestURI().getQuery();
+            Map<String, String> args = queryToMap(query);
+            String response = "###\n";
             for (Map.Entry e : args.entrySet()) {
-				response += e.getKey() + "\t" + e.getValue() + "\n";
-			}
-			response += "###";
+                    response += e.getKey() + "\t" + e.getValue() + "\n";
+            }
+            response += "###";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
@@ -35,21 +35,21 @@ public class WebServer {
         }
     }
 
-	private static Map<String, String> queryToMap(String query){
+    private static Map<String, String> queryToMap(String query){
 
-	    Map<String, String> result = new HashMap<String, String>();
-		if (query == null || query.length() == 0) {
-			return result;	
-		}
-	    for (String param : query.split("&")) {
-			String pair[] = param.split("=");
-			if (pair.length>1) {
-				result.put(pair[0], pair[1]);
-			}else{
-				result.put(pair[0], "");
-			}
-	    }
-	    return result;
-	}
+        Map<String, String> result = new HashMap<String, String>();
+        if (query == null || query.length() == 0) {
+                return result;
+        }
+        for (String param : query.split("&")) {
+                String pair[] = param.split("=");
+                if (pair.length>1) {
+                        result.put(pair[0], pair[1]);
+                }else{
+                        result.put(pair[0], "");
+                }
+        }
+        return result;
+    }
 
 }
