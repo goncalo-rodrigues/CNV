b66d3335fa043200408fa3c4cdd9c9098daa62f6
DiogoMRSilva <diogo.m.r.silva@ist.utl.pt>
2017-05-14 19:59:06 +0100

    added teacher example requests

--- previsionAlgorithmJava/src/ist/cnv/Main.java
+++ previsionAlgorithmJava/src/ist/cnv/Main.java
@@ -9,13 +9,25 @@ public class Main {
         imagesNames.add("img1");
         imagesNames.add("img2");
 	    PrevisionAlgorithm oracle = new PrevisionAlgorithm(imagesNames);
-        System.out.println(oracle.estimateCost("img1", 100, 100,
-                100,100, 0,0));
+	    //Light request
+        System.out.println(oracle.estimateCost("img1",400,300,400,
+                300, 0,0));
+        oracle.insertData("img1",400,300,400,
+                300, 0,0,1000);
 
-        oracle.insertData("img1",50,50,100,
-        100, 0,0,100000);
-        oracle.insertData("img1",50,50,100,
-                100, 0,0,100000);
+        //Medium request
+        System.out.println(oracle.estimateCost("img1",8000,6000,1600,
+                1200, 0,6400));
+                oracle.insertData("img1",8000,6000,1600,
+                1200, 0,6400 ,100000);
+
+        //Heavy request
+        System.out.println(oracle.estimateCost("img1",20000,15000,3250,
+                2500, 5000,6500));
+        oracle.insertData("img1",20000,15000,3250,
+                2500, 5000,6500 ,100000);
+
+
         oracle.insertData("img4",50,50,100,
                 100, 0,0,100000);
         oracle.insertData("img2",8,8,100,
