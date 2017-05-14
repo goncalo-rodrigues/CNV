03a0e7112d071b1a9b102cc86ce4ce682c698da6
DiogoMRSilva <diogo.m.r.silva@ist.utl.pt>
2017-05-14 18:50:18 +0100

    worker factory working properly and load balancer using it

--- previsionAlgorithmJava/src/ist/cnv/loadBalancer/RedirectRequest.java
+++ previsionAlgorithmJava/src/ist/cnv/loadBalancer/RedirectRequest.java
@@ -2,26 +2,36 @@ package ist.cnv.loadBalancer;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import ist.cnv.worker.AWSWorkerFactory;
+import ist.cnv.worker.Worker;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class RedirectRequest implements HttpHandler{
-    private List<AvailableWebServer> ws = new ArrayList<AvailableWebServer>();
+    private List<Worker> workers = new ArrayList<Worker>();
+    private AWSWorkerFactory workerFactory;
+
+    public RedirectRequest(){
+        workerFactory = new AWSWorkerFactory();
+    }
 
     @Override
     public void handle(HttpExchange t) {
         // Decides which WebServer will handle the request
-        if(ws.size() == 0)
-            // TODO: Check for the available instances
+        if(workers.size() == 0)
+            workers.add(workerFactory.createWorker());
 
-            // FIXME: Only for testing purposes
-            ws.add(new AvailableWebServer("localhost"));
-
+        String url = "http://" + choseWorkterToRequest();
 
         // Sends the request and waits for the result
-        // FIXME: For testing purposes, using the first element on the list
-        ContactChosenWSThread cct = new ContactChosenWSThread(t, ws.get(0).getUrl());
+        ContactChosenWSThread cct = new ContactChosenWSThread(t, url);
         Thread thread = new Thread(cct);
         thread.start();
+    }
+
+    private String choseWorkterToRequest(){//TODO set a proper chosing method
+        return workers.get(0).getAddress();
+
     }
 }
