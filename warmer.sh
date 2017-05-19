5e929e8351a186c1b565a1b2538cd894ef287ab9
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2017-05-19 19:14:17 +0100

    Added some script

--- warmer.sh
+++ warmer.sh
@@ -1,13 +1,13 @@
 address=$1
 
 for fn in "test01.txt" "test02.txt" "test03.txt" "test04.txt" "test05.txt"
-do 
+do
 	for i in 0 100 200 300
-	do 
+	do
 		for j in 0 100 200 300
 		do
-		curl http://$address:8000/r.html?f=$fn\&sc=400\&sr=400\&wc=100\&wr=100\&coff=$i\&roff=$j & 
-		sleep 0.1
+		curl http://$address:8000/r.html?f=$fn\&sc=400\&sr=400\&wc=100\&wr=100\&coff=$i\&roff=$j &
+		sleep 0.3
 		#sleep 0.05
 		done
 	done
