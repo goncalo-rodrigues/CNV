79a5479e59ebf62b80674ff080bdef905f3ffa0a
goncalo-rodrigues <metarl.kurtcoban@gmail.com>
2017-05-21 21:34:33 +0100

    evaluation graphs

--- light.sh
+++ light.sh
@@ -1,5 +1,9 @@
-for i in {1..500}
+while true
 do
-curl "http://127.0.0.1:8000/r.html?f=test04.txt&sc=400&sr=300&wc=400&wr=300&coff=0&roff=0" &
-sleep 0.1
+(
+start="$(date +%s)"
+curl "http://127.0.0.1:8000/r.html?f=test01.txt&sc=400&sr=300&wc=400&wr=300&coff=0&roff=0"  &>/dev/null
+echo $start,`(date +%s)` >> results.txt
+ )&
+sleep 1
 done
